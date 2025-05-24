c1b746a299744971a1c89da1ad384029b81ee2b5
VAN-2831 <abiabdulah911@gmail.com>
2025-05-24 23:19:16 +0700

    update dhcp & ftp

--- menubar.js
+++ menubar.js
@@ -4,9 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
   aside.innerHTML = `
     <h1 class="text-xl font-bold">Menu</h1>
     <nav class="space-y-2">
-      <a href="../index.html" class="block hover:bg-gray-700 p-2 rounded">Dashboard</a>
+      <a href="index.html" class="block hover:bg-gray-700 p-2 rounded">Dashboard</a>
       <a href="instalasi.html" class="block hover:bg-gray-700 p-2 rounded">Instalasi Debian</a>
       <a href="web-server.html" class="block hover:bg-gray-700 p-2 rounded">Web Server</a>
+      <a href="dhcp-server.html" class="block hover:bg-gray-700 p-2 rounded">DHCP Server</a>
+      <a href="ftp-server.html" class="block hover:bg-gray-700 p-2 rounded">FTP Server</a>
     </nav>
   `;
   document.body.prepend(aside);
